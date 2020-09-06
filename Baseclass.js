4378b08bcdc273ea73a9392cc4bed9cf17166442
Hardik <hardikwarialani2006@gmail.com>
2020-09-06 09:39:48 +0530

    firstfile

---
+++ Baseclass.js
@@ -0,0 +1,26 @@
+class Baseclass {
+    constructor(x, y, width, height,angle) {
+      var options = {
+          'restitution':0.8,
+          'friction':1.0,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+      this.image=loadImage("sprites/base.png")
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+     imageMode(CENTER);
+     image(this.image,0,0,this.width,this.height)
+      pop();
+    }
+  };
+  
