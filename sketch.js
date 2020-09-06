e0d73db073c65e5449c7bd4dc10fa42916f7f840
Hardik <hardikwarialani2006@gmail.com>
2020-09-06 09:48:38 +0530

    platformchange

--- sketch.js
+++ sketch.js
@@ -27,7 +27,7 @@ function setup(){
     pig3 = new Pig(810, 220);
 
     log3 =  new Log(810,180,300, PI/2);
-    platform = new Ground(120,320,300,150)
+    platform = new Ground(120,520,300,150)
 
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
